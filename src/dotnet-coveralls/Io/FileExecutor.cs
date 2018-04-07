8cd02ba918de1ae8bc362524b2109a76162db46e
Abraham Heidebrecht <abe545@gmail.com>
2018-04-06 23:27:05 -0400

    Added file executor

---
+++ src/dotnet-coveralls/Io/FileExecutor.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet.Coveralls.Io
+{
+    public class FileExecutor
+    {
+        public virtual async Task<(string StandardOut, string StandardErr, int ReturnCode)> Execute(ProcessStartInfo processStartInfo)
+        {
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.UseShellExecute = false;
+
+            var stdOut = new StringBuilder();
+            var stdErr = new StringBuilder();
+
+            using (var process = Process.Start(processStartInfo))
+            {
+                process.OutputDataReceived += (_, e) => stdOut.AppendLine(e.Data);
+                process.BeginOutputReadLine();
+
+                process.ErrorDataReceived += (_, e) => stdErr.AppendLine(e.Data);
+                process.BeginErrorReadLine();
+
+                await Task.Run(() => process.WaitForExit());
+
+                return (stdOut.ToString().NullIfEmpty(), stdErr.ToString().NullIfEmpty(), process.ExitCode);
+            }
+        }
+    }
+}
---
+++ test/dotnet-coveralls.Tests/IO/FileExecutorTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Dotnet.Coveralls.Io;
+using Machine.Specifications;
+using Shouldly;
+
+namespace Dotnet.Coveralls.Tests.IO.FileExecutorTests
+{
+    [Subject(typeof(FileExecutor))]
+    public class when_the_process_exits_successfully : with_file_executor
+    {
+        Establish context = () => ProcessStartInfo = new ProcessStartInfo("dotnet", "--version");
+
+        It should_return_0 = () => ExitCode.ShouldBe(0);
+        It should_not_have_stderr = () => StandardError.ShouldBeNull();
+        It should_return_stdout = () => StandardOut.ShouldNotBeNullOrWhiteSpace();
+    }
+
+    [Subject(typeof(FileExecutor))]
+    public class when_the_process_fails : with_file_executor
+    {
+        Establish context = () => ProcessStartInfo = new ProcessStartInfo("dotnet", "--invalid-argument");
+
+        It should_return_1 = () => ExitCode.ShouldBe(1);
+        It should_return_stderr = () => StandardError.ShouldNotBeNullOrWhiteSpace();
+    }
+
+    public class with_file_executor
+    {
+        Establish context = () => Subject = new FileExecutor();
+
+        Because of = () => (StandardOut, StandardError, ExitCode) = Subject.Execute(ProcessStartInfo).Result;
+
+        protected static FileExecutor Subject;
+        protected static ProcessStartInfo ProcessStartInfo;
+        protected static int ExitCode;
+        protected static string StandardError;
+        protected static string StandardOut;
+    }
+}
